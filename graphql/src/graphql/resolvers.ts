25fdb05ddce731eda33bce037e6f1c456c387592
Alfian Pramudita <alfian.pramudita@gmail.com>
2021-06-10 09:23:00 +0700

    feat: add sale order schema, query, and resolver

--- graphql/src/graphql/resolvers.ts
+++ graphql/src/graphql/resolvers.ts
@@ -1,6 +1,12 @@
 const resolvers = {
   Query: {
     hello: () => 'world',
+    saleOrders: () => {
+      return { nodes: [] };
+    },
+    saleOrder: (_root: any, _args: { id: number }) => {
+      return null;
+    },
   },
 };
 

8c6ea19fd7d48370b02100e9769cf8c3028e8c3c
Alfian Pramudita <alfian.pramudita@gmail.com>
2021-06-10 22:00:30 +0700

    chore: add trace to grpc handlers

--- grpc/pkg/saleorder/grpc.go
+++ grpc/pkg/saleorder/grpc.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"time"
 
+	"github.com/opentracing/opentracing-go"
 	"github.com/taogilaaa/trace-sandbox/grpc/internal/proto/sandbox_sales_v1"
 	"google.golang.org/protobuf/types/known/timestamppb"
 )
@@ -17,6 +18,9 @@ func NewGRPCServer() *grpcServer {
 }
 
 func (gs *grpcServer) GetSaleOrder(ctx context.Context, in *sandbox_sales_v1.GetSaleOrderRequest) (*sandbox_sales_v1.GetSaleOrderResponse, error) {
+	span, _ := opentracing.StartSpanFromContext(ctx, "grpc.GetSaleOrder")
+	defer span.Finish()
+
 	return &sandbox_sales_v1.GetSaleOrderResponse{
 		SaleOrder: &sandbox_sales_v1.SaleOrder{
 			Id:            1,
@@ -29,12 +33,18 @@ func (gs *grpcServer) GetSaleOrder(ctx context.Context, in *sandbox_sales_v1.Get
 }
 
 func (gs *grpcServer) GetSaleOrders(ctx context.Context, in *sandbox_sales_v1.GetSaleOrdersRequest) (*sandbox_sales_v1.GetSaleOrdersResponse, error) {
+	span, _ := opentracing.StartSpanFromContext(ctx, "grpc.GetSaleOrders")
+	defer span.Finish()
+
 	return &sandbox_sales_v1.GetSaleOrdersResponse{
 		SaleOrders: []*sandbox_sales_v1.SaleOrder{},
 	}, nil
 }
 
 func (gs *grpcServer) CreateSaleOrder(ctx context.Context, in *sandbox_sales_v1.CreateSaleOrderRequest) (*sandbox_sales_v1.CreateSaleOrderResponse, error) {
+	span, _ := opentracing.StartSpanFromContext(ctx, "grpc.CreateSaleOrder")
+	defer span.Finish()
+
 	return &sandbox_sales_v1.CreateSaleOrderResponse{
 		SaleOrder: &sandbox_sales_v1.SaleOrder{
 			Id:            1,

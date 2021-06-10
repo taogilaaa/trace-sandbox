c2f90090de438b09743169139299b1af4532c3a1
Alfian Pramudita <alfian.pramudita@gmail.com>
2021-06-10 09:37:40 +0700

    feat: add createSaleOrder mutation

--- graphql/src/graphql/resolvers.ts
+++ graphql/src/graphql/resolvers.ts
@@ -1,3 +1,9 @@
+type CreateSaleOrderArgs = {
+  email: string;
+  paymentMethod: 'cash' | 'cashless';
+  products: Array<{ name: string; quantity: string }>;
+};
+
 const resolvers = {
   Query: {
     hello: () => 'world',
@@ -6,6 +12,15 @@ const resolvers = {
     },
     saleOrder: (_root: any, _args: { id: number }) => {
       return null;
+    },
+  },
+  Mutation: {
+    createSaleOrder: (_root: any, args: CreateSaleOrderArgs) => {
+      return {
+        ...args,
+        id: 1,
+        orderDate: new Date().toISOString(),
+      };
     },
   },
 };

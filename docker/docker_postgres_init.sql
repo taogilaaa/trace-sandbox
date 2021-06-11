d0363e2659ee8e0f2799b60151c4cfdddeb56bd9
Alfian Pramudita <alfian.pramudita@gmail.com>
2021-06-11 07:27:10 +0700

    fix: remove manual id assign on serial primary keys

--- docker/docker_postgres_init.sql
+++ docker/docker_postgres_init.sql
@@ -13,15 +13,15 @@ CREATE TABLE sale_order_product (
   quantity int NOT NULL
 );
 
-INSERT INTO sale_order(id, email, payment_method, order_date) VALUES
- (1, 'michaelsuyama@northwind.com', 'cash', '2021-06-10'),
- (2, 'nancydavolio@northwind.com', 'cashless', '2021-06-10'),
- (3, 'davidbuchanan@northwind.com', 'cash', '2021-06-10');
+INSERT INTO sale_order(email, payment_method, order_date) VALUES
+ ('michaelsuyama@northwind.com', 'cash', '2021-06-10'),
+ ('nancydavolio@northwind.com', 'cashless', '2021-06-10'),
+ ('davidbuchanan@northwind.com', 'cash', '2021-06-10');
 
-INSERT INTO sale_order_product(id, sale_order_id, name, quantity) VALUES
- (1, 1, 'Chicken', 1),
- (2, 1, 'Pepsi', 3),
- (3, 1, 'Momogi', 10),
- (4, 2, 'Coca Cola', 5),
- (5, 2, 'Mint Candy', 200),
- (6, 3, 'Canned Coffee', 10);
+INSERT INTO sale_order_product(sale_order_id, name, quantity) VALUES
+ (1, 'Chicken', 1),
+ (1, 'Pepsi', 3),
+ (1, 'Momogi', 10),
+ (2, 'Coca Cola', 5),
+ (2, 'Mint Candy', 200),
+ (3, 'Canned Coffee', 10);
